469a1917ee666946d157b922723fdbd5d2197722
J Phaiboon <phaiboon760@gmail.com>
2023-06-02 22:00:32 +0700

    improve algo

--- WEEK1/DAY1/TicTacToe.py
+++ WEEK1/DAY1/TicTacToe.py
@@ -25,32 +25,32 @@ def algo():
     V = [-100]*9
     for m in validMove:
         tempX = X + [m]
-        V[m],forceMove = evalOX(O,tempX)
-        if(len(forceMove)>0):
-            move = [i for i in forceMove if i in validMove]
-            return random.choice(move)
+        if checkWin(tempX):  # If can win with this move, do it
+            return m
+    for m in validMove:
+        tempO = O + [m]
+        if checkWin(tempO):  # If opponent can win with this move, block it
+            return m
+    for m in validMove:  # Original strategy
+        tempX = X + [m]
+        V[m] = evalOX(O,tempX)
     maxV = max(V)
     imaxV = [i for i, j in enumerate(V) if j == maxV]
     return random.choice(imaxV)
-        
+
 def evalOX(O,X):
-    SO,SX,forceMove = calSOX(O,X)
-    return 1+SX-SO,forceMove
+    SO,SX = calSOX(O,X)
+    return 1+SX-SO
 def calSOX(O,X):
-    SO = 0
-    SX = 0
-    forceMove = []
+    SO = SX = 0
     for w in win:
         o = [i in O for i in w]
         x = [i in X for i in w]
         if not any(x):
-            nO = o.count(True)
-            SO += nO
-            if nO == 2:
-                forceMove = w
+            SO += o.count(True)
         if not any(o):
             SX += x.count(True)
-    return SO,SX,forceMove
+    return SO,SX
     
 if __name__ == "__main__":
     print("Welcome to XO game")
@@ -64,18 +64,18 @@ if __name__ == "__main__":
     print(" 6 | 7 | 8 ")
     print("Input -1 to exit")
     while True:
-        # move = input("Your move (enter a number 0-8): ")
-        # try:
-        #     move = int(move)
-        #     if move == -1:
-        #         break
-        #     if move not in range(9) or move in O+X:
-        #         print("Invalid move, please try again.")
-        #         continue
-        # except ValueError:
-        #     print("Invalid move, please try again.")
-        #     continue
-        O.append(algo())
+        move = input("Your move (enter a number 0-8): ")
+        try:
+            move = int(move)
+            if move == -1:
+                break
+            if move not in range(9) or move in O+X:
+                print("Invalid move, please try again.")
+                continue
+        except ValueError:
+            print("Invalid move, please try again.")
+            continue
+        O.append(move) # if optimal vs optimal expected : draw
         if checkWin(O):
             print("You win")
             break
@@ -83,7 +83,7 @@ if __name__ == "__main__":
             print("Draw")
             break
         X.append(algo())
-        displayOX()
+        # displayOX()
         if checkWin(X):
             print("I win")
             break
